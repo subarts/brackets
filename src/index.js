1447ee8229c3f49b6160cb227d0fe29f1a629260
artem subbotin <subbota1991@gmail.com>
2022-08-04 16:46:02 +0400

    init:start task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig,brack= false) {
+   let string="";
+   if(str.length==0) return brack
+   for(let i=0;i<bracketsConfig.length;i++){
+    let bracket= bracketsConfig[i].reduce((a,b)=>a+b)
+    let s=str.indexOf(bracket)
+    brack=str.includes(bracket)
+    string= brack? str.slice(0,s) +str.slice(s+2):""
+    if(brack) break;
+  }
+  return check(string, bracketsConfig, brack)
 }
